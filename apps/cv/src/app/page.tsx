f0d9958c1788f2df75246c1638a12e0258ce1eca
alessiotortora <alessiotortora@gmail.com>
2025-04-26 15:35:27 +0200

    repo init

---
+++ apps/cv/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+	return (
+		<div>
+			<Button appName="cv">Click me</Button>
+		</div>
+	);
+}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/ui/button";
+
+function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <Button appName="web">Click me</Button>
+    </>
+  );
+}
+
+export default Home;
