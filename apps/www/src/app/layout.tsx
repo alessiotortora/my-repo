548437cd4a7d18726aeb75ce367f20beee23db49
alessiotortora <alessiotortora@gmail.com>
2025-11-01 17:04:03 +0100

    added openpanel for tracking

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter, Newsreader } from "next/font/google";
 import localFont from "next/font/local";
 import "@repo/ui/globals.css";
 import { ConsoleArt } from "@repo/ui/components/console-art";
-import Script from "next/script";
+import { OpenPanelComponent } from '@openpanel/nextjs';
 
 const inter = Inter({
   variable: "--font-primary",
@@ -33,6 +33,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <OpenPanelComponent
+        clientId='1e3d9bcf-a30b-43c6-b9d9-c49430003861'
+        trackScreenViews={true}
+        trackOutgoingLinks={true}
+        trackAttributes={true}
+      />
+  
       <body
         className={`${inter.variable} ${newsreader.variable}  ${alessioScript.variable} antialiased`}
       >
